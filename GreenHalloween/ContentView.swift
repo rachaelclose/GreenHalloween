5a35f7c4f2d825434f9e6d690ebc0e8df3641c08
Rachael Close <rlc355@cornell.edu>
2022-10-15 13:34:01 -0400

    Initial Commit

---
+++ GreenHalloween/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GreenHalloween
+//
+//  Created by Rachael Close on 10/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GreenHalloween/GreenHalloweenApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenHalloweenApp.swift
+//  GreenHalloween
+//
+//  Created by Rachael Close on 10/15/22.
+//
+
+import SwiftUI
+
+@main
+struct GreenHalloweenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
